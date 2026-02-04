b71ee1fed02ec48cbacaf98bed507c6ca0543668
Vivek Kumar <viveksingh599999@gmail.com>
2026-02-04 18:13:19 +0530

    Implemented Interface Templeate and Abstraction

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 //Functions
 function ProductDetails(id, name) {
     if (name == undefined) {
@@ -44,3 +59,33 @@ var obj2 = new StaticNonStatic();
 obj2.Print();
 var obj3 = new StaticNonStatic();
 obj3.Print();
+//Template
+var ProductTemplate = /** @class */ (function () {
+    function ProductTemplate() {
+        this.Name = null;
+        this.Price = 0;
+        this.Qty = 0;
+    }
+    return ProductTemplate;
+}());
+//Component
+var ProductComponent = /** @class */ (function (_super) {
+    __extends(ProductComponent, _super);
+    function ProductComponent() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.Name = "Samsung TV";
+        _this.Price = 45000.44;
+        _this.Qty = 2;
+        return _this;
+    }
+    ProductComponent.prototype.Total = function () {
+        return this.Qty * this.Price;
+    };
+    ProductComponent.prototype.Print = function () {
+        console.log("Name = ".concat(this.Name, "\nPrice = ").concat(this.Price, "\nQty = ").concat(this.Qty, "\nTotal = ").concat(this.Total()));
+    };
+    return ProductComponent;
+}(ProductTemplate));
+//Instance
+var obj4 = new ProductComponent();
+obj4.Print();
--- src/index.ts
+++ src/index.ts
@@ -34,11 +34,11 @@ interface Bank_Version1 {
 interface Bank_Version2 extends Bank_Version1 {
     Loan: boolean;
 }
-let bank_legacy:Bank_Version1 = {
+let bank_legacy: Bank_Version1 = {
     NRI: "Abc",
     Personal: "lmn"
 }
-let bank_new:Bank_Version2 = {
+let bank_new: Bank_Version2 = {
     NRI: "xyz",
     Personal: "jkl",
     Loan: true
@@ -64,3 +64,38 @@ let obj2 = new StaticNonStatic();
 obj2.Print();
 let obj3 = new StaticNonStatic();
 obj3.Print();
+
+//Contract Interface
+interface ProductContract1 {
+    Name: string | null;
+    Price: number;
+    Qty: number;
+    Total(): number;
+    Print(): void;
+}
+
+//Template
+abstract class ProductTemplate implements ProductContract1 {
+    public Name: string | null = null;
+    public Price: number = 0;
+    public Qty: number = 0;
+    public abstract Total(): number;
+    public abstract Print(): void;
+}
+
+//Component
+class ProductComponent extends ProductTemplate {
+    Name = "Samsung TV";
+    Price = 45000.44;
+    Qty = 2;
+    Total() {
+        return this.Qty * this.Price;
+    }
+    Print() {
+        console.log(`Name = ${this.Name}\nPrice = ${this.Price}\nQty = ${this.Qty}\nTotal = ${this.Total()}`);
+    }
+}
+
+//Instance
+let obj4 = new ProductComponent();
+obj4.Print();
