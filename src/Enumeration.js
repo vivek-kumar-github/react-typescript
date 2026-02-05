7e10784f9680198bdb83d89c2105a90e1afa9d83
Vivek Kumar <viveksingh599999@gmail.com>
2026-02-05 09:00:22 +0530

    Implemented Enumeration enum

---
+++ src/Enumeration.js
@@ -0,0 +1,29 @@
+var Weekdays;
+(function (Weekdays) {
+    Weekdays[Weekdays["Sunday"] = 0] = "Sunday";
+    Weekdays[Weekdays["Monday"] = 1] = "Monday";
+    Weekdays[Weekdays["Tuesday"] = 2] = "Tuesday";
+    Weekdays[Weekdays["Wednesday"] = 3] = "Wednesday";
+    Weekdays[Weekdays["Thursday"] = 4] = "Thursday";
+    Weekdays[Weekdays["Friday"] = 5] = "Friday";
+    Weekdays[Weekdays["Saturday"] = 6] = "Saturday";
+})(Weekdays || (Weekdays = {}));
+var Months;
+(function (Months) {
+    Months[Months["Jan"] = 0] = "Jan";
+    Months[Months["Feb"] = 1] = "Feb";
+    Months[Months["Mar"] = 2] = "Mar";
+    Months[Months["Apr"] = 3] = "Apr";
+    Months[Months["May"] = 4] = "May";
+    Months[Months["Jun"] = 5] = "Jun";
+    Months[Months["Jul"] = 6] = "Jul";
+    Months[Months["Aug"] = 7] = "Aug";
+    Months[Months["Sep"] = 8] = "Sep";
+    Months[Months["Oct"] = 9] = "Oct";
+    Months[Months["Nov"] = 10] = "Nov";
+    Months[Months["Dec"] = 11] = "Dec";
+})(Months || (Months = {}));
+var now = new Date();
+console.log("\n    Month : ".concat(now.getMonth(), "\n\n    Weekday : ").concat(now.getDay(), "\n\n    "));
+//enum Reverse Mapping 
+console.log("\n    Weekday: ".concat(Weekdays[now.getDay()], "\n\n    Month: ").concat(Months[now.getMonth()], "\n    "));
---
+++ src/Enumeration.ts
@@ -0,0 +1,34 @@
+enum Weekdays {
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+enum Months {
+    Jan,
+    Feb,
+    Mar,
+    Apr,
+    May,
+    Jun,
+    Jul,
+    Aug,
+    Sep,
+    Oct,
+    Nov,
+    Dec
+}
+let now: any = new Date();
+console.log(`
+    Month : ${now.getMonth()}\n
+    Weekday : ${now.getDay()}\n
+    `)
+
+//enum Reverse Mapping 
+console.log(`
+    Weekday: ${Weekdays[now.getDay()]}\n
+    Month: ${Months[now.getMonth()]}
+    `)
